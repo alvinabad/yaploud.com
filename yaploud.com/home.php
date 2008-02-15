514b2e3c330a070eeda1af72c7bafe3c1dd81bae
alvin <alvinabad@users.noreply.github.com>
2008-02-15 03:39:20 +0000

    new look for home page

--- yaploud.com/home.php
+++ yaploud.com/home.php
@@ -1,61 +1,163 @@
-<?php
-   //require_once 'db.inc';
-   //ob_start();
-   //session_start();
-   require("./user_session_init_c.inc");
-   //include 'common/header.php';
-   include 'common/header2.php';
-?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.yaploud.com/TR/html4/loose.dtd">
 
 <html>
+
+<?php
+require("./user_session_init_c.inc");
+?>
+
+
 <head>
-<title>YapLoud.com</title>
 <link rel="stylesheet" type="text/css" href=css/chat.css />
-<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.3.1/build/container/assets/container.css">
-<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.3.1/build/assets/skins/sam/skin.css">
+<link rel="stylesheet" type="text/css" href="css/tap_style.css">
+<style>
+label {
+	font: 18px Helvetica, Arial, sans-serif;
+	text-align: right;
+	display: block;
+	width: 50px;
+}
 
-
+input {
+	font: 18px Helvetica, Arial, sans-serif;
+	color: orange;
+}
+</style>
 </head>
-<body style="font-size:16px;" class=yui-skin-sam>
-<?php include 'common/yui.php'; ?>
+<body>
+<?php include("common/header2.php");  ?>
 
-<script type=text/javascript src=js/chat.js></script>
-<?
-   $sess = $_SESSION;
-   if(!$sess['logged']){
-      print "<h2><a href=login_page.php>Please Login</a><br/></h2><br/>\n";
-      ob_end_flush();
-      return;
-   }
-   $username = $_SESSION['username'];
-   print "<h3><b>Hello, $username</b></h3><br/>\n";
-?>
 
-   <div id=popular_chat>
-   	<div class=hd>Popular Chat Sites</div>
-	<div class=bd>
-<?
-   $query = "select topic_url as url, count(*) as c, (select count(username) from dev.room_users where topic_url = url) as uniqs  from dev.chat group by topic_url order by 2 desc limit 15";
-   $db = new DB();
-   $result = $db->mysql_query($query);
-   if($result){
-      while($row = mysql_fetch_assoc($result)){
-         print "<div class=home_row><span class=column>" . $row['c'] . "&nbsp;msgs</span>&nbsp;<span class=column>" .  $row['uniqs'] . "&nbsp;users</span>&nbsp;<a style=\"font-size:14px;width:450px;float:left;text-align:right;\" href=chat_frames.php?url=http://" . urlencode($row['url']) . ">" . $row['url'] . "</a></div>\n";
-      }
-      mysql_free_result($result);
-   }
-?>
-	</div>
-	<div class=ft></div>
-   </div>
-   <script>
-      var popular_panel = new YAHOO.widget.Panel('popular_chat', {width:'600px', visible:true, draggable:false, close:false});
-      popular_panel.render();
-   </script>
+<div style="clear: both;"></div>
 
-<?
-   ob_end_flush();
-?>
+<p />
+
+
+<div style="width: 550px; float: left;">
+<table width="777" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<th width="100%" align="left" valign="top" scope="col">
+		<table width="99%" border="0" cellspacing="0" cellpadding="0">
+			<tr>
+				<td>
+				<form method=get action="/chat.php" onsubmit="strip_http(); "><span
+					style="font: 20px Helvetica, Arial, sans-serif; color: #336600; font-weight: bold;">Enter
+				URL to Yap:</span> <input name="url" id="yapurl_box" type="text"
+					class="Text2_b" value="http://" size="40"></input> <input
+					type="submit" value="Go" class="Text2_b" /></form>
+				</td>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+		</th>
+	
+	
+	<tr>
+
+</table>
+<p />
+<p />
+
+
+<div id=popular_chat>
+
+<table>
+	<tr align="left" valign="middle" height="23px" class="grry_line">
+		<td><span class="arial_16">Popular</span></td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>
+		<table width="0" border="0" cellspacing="0" cellpadding="0">
+			<tr>
+				<td class="gr_text"></td>
+				<td>
+				<div align="right" class="view_details_text">
+				<div align="center"><a href="#">Today</a> | <a href="#">This Week</a>
+				| <a href="#">Ever</a>
+				</div>
+				</div>
+				</td>
+			</tr>
+		</table>
+		</td>
+	</tr>
+
+	<?
+	$query = "select topic_url as url, count(*) as c, (select count(username) from dev.room_users where topic_url = url) as uniqs  from dev.chat group by topic_url order by 2 desc limit 15";
+	$db = new DB();
+	$result = $db->mysql_query($query);
+	if($result){
+		while($row = mysql_fetch_assoc($result)){
+			echo<<<HTML
+                <tr height="30px" class="grry_line2">
+                    <td width="20px"><span class="users">
+HTML;
+			print $row['uniqs'];
+			echo <<<HTML
+        </span><span class="gr_text">yappers</span></td>
+                    <td align="right"><span class="mail_text">
+HTML;
+			print '<a href="http://' . $row['url'] .
+              '">' . 'http://' . $row['url'] . '</a>';
+
+			echo <<<HTML
+        <img src="images/icon2.gif" width="10" height="10"> </span><span class="view_details_text">view details <img src="images/icon_new.gif" width="31" height="11"></span></td>
+                    <td><div align="right"><img src="images/icon1.gif" width="14" height="14"></div></td>
+                    <td><div align="center"><span class="view_details_text style3">
+HTML;
+			print $row['c'];
+			echo <<<HTML
+        </span></div></td>
+                    <td valign="middle"><div align="right"><img src="images/vote.gif" width="43" height="18"></div></td>
+                </tr>
+HTML;
+		}
+		mysql_free_result($result);
+	}
+	?>
+
+</table>
+</div>
+</div>
+
+<p />
+<p />
+
+<div style="width: 200px; padding: 10px 10px 5px 10px;">
+<table>
+	<tr>
+		<td>
+		<div align="center"><img src="images/plugin_firefox.gif" width="167"
+			height="30"></div>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		<div align="center"><img src="images/plugin_ie.gif" width="167"
+			height="30"></div>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		<div align="center"><img src="images/add.gif" width="182" height="143"></div>
+		</td>
+	</tr>
+</table>
+</div>
+
+<div style="clear: both;"></div>
+<p />
+
+
+<div align="left" class="main_text">Free. No spyware or adware.</div>
+
+	<?PHP include("common/footer.php"); ?>
 
 </body>
 </html>
+
+
+
+	<?php ob_end_flush(); ?>
