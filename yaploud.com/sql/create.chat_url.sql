608b2dfec69525dfd4a43e6d305143529c5353ad
alvin <alvinabad@users.noreply.github.com>
2008-02-25 09:52:36 +0000

    Added votes, rating columns

--- yaploud.com/sql/create.chat_url.sql
+++ yaploud.com/sql/create.chat_url.sql
@@ -3,9 +3,11 @@ CREATE TABLE chat_url (
    url varchar(2096) NOT NULL UNIQUE,
    title varchar(4096),
    description TEXT,
+   votes int,
+   average_rating float,
    creation_date datetime NOT NULL,
    FULLTEXT(title, description),
    PRIMARY KEY (id)
 ) type = MyISAM;
 
-create index creation_date_idx on dev.chat (creation_date);
+create index chat_url_creation_date_idx on chat_url (creation_date);
