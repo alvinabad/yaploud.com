a297b3fb677b0d28ef09931fb91c5cabeb2e9aaa
alvin <alvinabad@users.noreply.github.com>
2008-02-25 08:49:58 +0000

    Moved sql scripts to sql directory

--- yaploud.com/create.room_users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE room_users (
-  username varchar(64) NOT NULL,
-  topic_url varchar(256) NOT NULL,
-  creation_date datetime NOT NULL,
-  session varchar(32) NOT NULL,
-  INDEX rooms_user_idx(username, topic_url)
-) type=MyISAM;
