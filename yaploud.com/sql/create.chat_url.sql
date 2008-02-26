d653f479656bc9abf746df006b16d5eeb8cc9c75
alvin <alvinabad@users.noreply.github.com>
2008-02-26 09:55:31 +0000

    Updated table

--- yaploud.com/sql/create.chat_url.sql
+++ yaploud.com/sql/create.chat_url.sql
@@ -1,7 +1,8 @@
+
 CREATE TABLE chat_url (
    id bigint NOT NULL AUTO_INCREMENT,
-   url varchar(2096) NOT NULL UNIQUE,
-   title varchar(4096),
+   url varchar(1000) NOT NULL UNIQUE,
+   title varchar(2048),
    description TEXT,
    votes int,
    average_rating float,
