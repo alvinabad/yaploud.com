a7bf093dff5e021c535795b97145d08e43ca718d
alvin <alvinabad@users.noreply.github.com>
2008-03-18 11:07:05 +0000

    Added javascript for widget

---
+++ yaploud.com/chat/chat_widget.js
@@ -0,0 +1,72 @@
+
+
+function init_panel() {
+    // Setup constants
+
+    // QUIRKS FLAG, FOR BOX MODEL
+    var IE_QUIRKS = (YAHOO.env.ua.ie && document.compatMode == "BackCompat");
+
+    // UNDERLAY/IFRAME SYNC REQUIRED
+    var IE_SYNC = (YAHOO.env.ua.ie == 6 || (YAHOO.env.ua.ie == 7 && IE_QUIRKS));
+
+    // PADDING USED FOR BODY ELEMENT (Hardcoded for example)
+    var PANEL_BODY_PADDING = (10*2) // 10px top/bottom padding applied to Panel body element. The top/bottom border width is 0
+
+    // Create a panel Instance, from the 'chat_panel' DIV standard module markup
+    var panel = new YAHOO.widget.Panel('main', {
+        draggable: true,
+        width: '320px',
+        //minWidth: '400px',
+        //xy: [800, 100],
+        fixedcenter: true,
+        constraintoviewport: true,
+        context: ["showbtn", "tl", "bl"]
+    });
+    
+    panel.render();
+    //panel.focus();
+
+/***
+    // Create Resize instance, binding it to the 'chat_panel' DIV 
+    var resize = new YAHOO.util.Resize('main2', {
+        handles: ['br'],
+        autoRatio: false,
+        minWidth: 320,
+        minHeight: 100,
+        status: true
+    });
+
+    // Setup resize handler to update the size of the Panel's body element
+    // whenever the size of the 'chat_panel' DIV changes
+    resize.on('resize', 
+        function(args) {
+            var panelHeight = args.height;
+
+	        var headerHeight = this.header.offsetHeight; // Content + Padding + Border
+	        var footerHeight = this.footer.offsetHeight; // Content + Padding + Border
+	
+	        var bodyHeight = (panelHeight - headerHeight - footerHeight);
+	        var bodyContentHeight = (IE_QUIRKS) ? bodyHeight : bodyHeight - PANEL_BODY_PADDING;
+	
+	        YAHOO.util.Dom.setStyle(this.body, 'height', bodyContentHeight + 'px');
+	
+	        if (IE_SYNC) {
+	            // Keep the underlay and iframe size in sync.
+	
+	            // You could also set the width property, to achieve the 
+	            // same results, if you wanted to keep the panel's internal
+	            // width property in sync with the DOM width. 
+	
+	            this.sizeUnderlay();
+	
+	            // Syncing the iframe can be expensive. Disable iframe if you
+	            // don't need it.
+	            this.syncIframe();
+	        }
+        }, panel, true);
+
+    YAHOO.util.Event.on("showbtn", "click", panel.show, panel, true);
+    ***/
+}
+    
+//YAHOO.util.Event.onDOMReady(init_panel);
---
+++ yaploud.com/chat/yui.php
@@ -0,0 +1,31 @@
+
+<link rel="stylesheet" type="text/css" href="/js/yui/build/fonts/fonts-min.css" />
+<link rel="stylesheet" type="text/css" href="/js/yui/build/container/assets/skins/sam/container.css">
+<script type="text/javascript" src="/js/yui/build/yahoo-dom-event/yahoo-dom-event.js"></script>
+<script type="text/javascript" src="/js/yui/build/dragdrop/dragdrop-min.js"></script>
+<script type="text/javascript" src="/js/yui/build/container/container-min.js"></script>
+
+<script type="text/javascript" src="/js/yui/build/yahoo/yahoo-min.js"></script> 
+<script type="text/javascript" src="/js/yui/build/event/event-min.js"></script> 
+<script src="/js/yui/build/connection/connection-min.js"></script>
+
+
+<?php
+/**
+<link
+    rel="stylesheet" type="text/css"
+    href="/js/yui/build/resize/assets/skins/sam/resize.css" />
+
+
+<script
+    type="text/javascript" src="/js/yui/build/animation/animation-min.js"></script>
+
+
+<script
+    type="text/javascript" src="/js/yui/build/utilities/utilities.js"></script>
+
+
+<script
+    type="text/javascript" src="/js/yui/build/resize/resize-beta-min.js"></script>
+**/
+?>
