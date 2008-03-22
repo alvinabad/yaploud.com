3dd6ad60d829df87de057b21c171e711ba72f0d6
alvin <alvinabad@users.noreply.github.com>
2008-03-22 21:45:48 +0000

    Use URL in xpi

--- yaploud.com/extension/browser_extension.js
+++ yaploud.com/extension/browser_extension.js
@@ -2,24 +2,24 @@
 function install (aEvent) {
   var params = {
     "YapLoud": { URL: aEvent.target.href,
-             IconURL: aEvent.target.getAttribute("iconURL"),
+              IconURL: aEvent.target.getAttribute("iconURL"),
              Hash: aEvent.target.getAttribute("hash"),
              toString: function () { return this.URL; }
     }
   };
   params = { "YapLoud": aEvent.target.href };
   var xpi = "/extension/yaploud.xpi";
-  params = { "YapLoud": "/extension/yaploud.xpi" };
+  xpi = { "YapLoud": { URL: "/extension/yaploud.xpi" } };
   InstallTrigger.install(xpi);
   return false;
 }
 
 function installxpi (target) {
-  if (navigator.userAgent.indexOf('Firefox') != -1) {
-      var xpi = { "YapLoud": 'yaploud.xpi' };
-      InstallTrigger.install(xpi);
-  }
-  else {
-      alert("This extension works only on Firefox!");
-  }
+    var xpi = { "YapLoud": { URL: "/extension/yaploud.xpi" } };
+    if (navigator.userAgent.indexOf('Firefox') != -1) {
+        InstallTrigger.install(xpi);
+    }
+    else {
+        alert("This extension works only on Firefox!");
+    }
 }
