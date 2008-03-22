8c5ecad6e1e95b2baf74d324f8c8496e09093d2f
alvin <alvinabad@users.noreply.github.com>
2008-03-22 10:40:00 +0000

    Added source for dialog functionality

--- yaploud.com/chat/yui.php
+++ yaploud.com/chat/yui.php
@@ -1,7 +1,12 @@
 
 <link rel="stylesheet" type="text/css" href="/js/yui/build/fonts/fonts-min.css" />
+<link rel="stylesheet" type="text/css" href="/js/yui/build/button/assets/skins/sam/button.css" />
 <link rel="stylesheet" type="text/css" href="/js/yui/build/container/assets/skins/sam/container.css">
 <link rel="stylesheet" type="text/css" href="/js/yui/build/resize/assets/skins/sam/resize.css" />
+
+<script type="text/javascript" src="/js/yui/build/utilities/utilities.js"></script>
+<script type="text/javascript" src="/js/yui/build/button/button-min.js"></script>
+<script type="text/javascript" src="/js/yui/build/container/container-min.js"></script>
 
 <script type="text/javascript" src="/js/yui/build/yahoo-dom-event/yahoo-dom-event.js"></script>
 <script type="text/javascript" src="/js/yui/build/dragdrop/dragdrop-min.js"></script>
@@ -9,7 +14,7 @@
 
 <script type="text/javascript" src="/js/yui/build/yahoo/yahoo-min.js"></script> 
 <script type="text/javascript" src="/js/yui/build/event/event-min.js"></script> 
-<script src="/js/yui/build/connection/connection-min.js"></script>
+<script type="text/javascript" src="/js/yui/build/connection/connection-min.js"></script>
 
 <!-- Utility Dependencies -->
 <script type="text/javascript" src="/js/yui/build/element/element-beta-min.js"></script> 
@@ -18,6 +23,7 @@
 
 <!-- Source file for the Resize Utility -->
 <script type="text/javascript" src="/js/yui/build/resize/resize-beta.js"></script>
+
 
 
 <?php
