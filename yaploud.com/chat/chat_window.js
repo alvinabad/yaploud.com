09191b741a2599e1e1cde10f69c8a1ac5e02dd3c
alvin <alvinabad@users.noreply.github.com>
2008-03-22 12:48:11 +0000

    Fixed logout.php typo

--- yaploud.com/chat/chat_window.js
+++ yaploud.com/chat/chat_window.js
@@ -363,7 +363,7 @@ var SendMessage_callback = {
 
 var SendLogout = {
     handleFailure:function(o){
-        alert('Sending logout failed ' + o.responseText + ' ' + o.status + );
+        alert('Sending logout failed: ' + o.responseText + ': ' + o.status);
     },
 
     handleSuccess:function(o){
