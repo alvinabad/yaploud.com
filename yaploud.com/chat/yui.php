6e61080349cca24b5da94da4ee68c3ada8096201
alvin <alvinabad@users.noreply.github.com>
2008-04-02 08:15:02 +0000

    Added YUI tab js

--- yaploud.com/chat/yui.php
+++ yaploud.com/chat/yui.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" type="text/css" href="/js/yui/build/button/assets/skins/sam/button.css" />
 <link rel="stylesheet" type="text/css" href="/js/yui/build/container/assets/skins/sam/container.css">
 <link rel="stylesheet" type="text/css" href="/js/yui/build/resize/assets/skins/sam/resize.css" />
+<link rel="stylesheet" type="text/css" href="/js/yui/build/tabview/assets/skins/sam/tabview.css">
 
 <script type="text/javascript" src="/js/yui/build/utilities/utilities.js"></script>
 <script type="text/javascript" src="/js/yui/build/button/button-min.js"></script>
@@ -24,7 +25,8 @@
 <!-- Source file for the Resize Utility -->
 <script type="text/javascript" src="/js/yui/build/resize/resize-beta.js"></script>
 
-
+<!-- Source file for TabView -->
+<script type="text/javascript" src="/js/yui/build/tabview/tabview-min.js"></script>
 
 <?php
 /**
