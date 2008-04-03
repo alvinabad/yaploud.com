a46833e2d43c5bf5b0cfb2aef14c26eeb639579b
alvin <alvinabad@users.noreply.github.com>
2008-04-03 00:57:01 +0000

    Moved old and dead code to Archives

--- yaploud.com/invitefriend.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $to = $_GET['to'];
-   $url = $_GET['url'];
-   $url = str_replace('http://', '', $url);
-   $url = str_replace('https://', '', $url);
-   mail($to, "Join Chat at Yaploud.com", "You have been invited to join the chat session about $url at Yaploud.com.  Click here to join http://www.yaploud.com/chat.php?url=" . $url);
-?>
