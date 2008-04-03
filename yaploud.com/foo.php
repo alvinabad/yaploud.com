8e4cf618852dec2b9ff8ac6b9d117d58e7648322
alvin <alvinabad@users.noreply.github.com>
2008-04-03 00:58:10 +0000

    Moved old and dead code to Archives

--- yaploud.com/foo.php
+++
@@ -1,9 +0,0 @@
-<script type=text/javascript>
-function cl(e){
-   for(i in e){
-      document.write(i + ' ' + e[i] + '<br/>');
-   }
-   e.returnValue = 'sure you want to leave yaploud?';
-}
-window.onbeforeunload = cl;
-</script>
