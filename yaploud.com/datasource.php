3ce1311415a69029476aa9cba781afb64989f16f
alvin <alvinabad@users.noreply.github.com>
2008-04-04 10:14:22 +0000

    revised to use private datasource

--- yaploud.com/datasource.php
+++ yaploud.com/datasource.php
@@ -1,17 +1,19 @@
 
 <?php
 
-if ($_SERVER['HTTP_HOST'] == "www.yaploud.com") {
-    // Dev
-    $ds['host'] = "west.yaploud.com";
-    $ds['user'] = "dev";
-    $ds['password'] = "cmuwest";
+// Create your own datasource credentials locally 
+$ds_local = ".local_datasource.php";
+
+// use it if it exists
+if (file_exists($_SERVER['DOCUMENT_ROOT']  . "/" . $ds_local) ) {
+    include($ds_local);
 }
 else {
-    // local sandbox
-    $ds['host'] = "localhost";
-    $ds['user'] = "yaploud";
-    $ds['password'] = "yaploud";
+	// Set your database credentials here
+	// Note: This will be shared by all developers
+    $ds['host'] = "";
+    $ds['user'] = "";
+    $ds['password'] = "";
 }
 
 ?>
