3e0914fe7c19afe4b49ba9e1298c78f2ed0d7358
alvin <alvinabad@users.noreply.github.com>
2008-04-09 05:40:21 +0000

    Changed to ADD UNIQUE for 2 fields to eliminate duplicate tag and urlid

--- yaploud.com/db/create.chat_url_tag.sql
+++ yaploud.com/db/create.chat_url_tag.sql
@@ -1,10 +1,12 @@
+DROP TABLE IF EXISTS chat_url_tag;
 CREATE TABLE chat_url_tag (
    id bigint NOT NULL AUTO_INCREMENT,
    chat_url_id integer not null,
-   tag varchar(1000) NOT NULL,
+   tag varchar(996) NOT NULL,
    FULLTEXT(tag),
    PRIMARY KEY (id)
 ) type = MyISAM;
 
-create unique index chat_url_tag_idx on chat_url_tag (tag);
+alter table chat_url_tag ADD UNIQUE (chat_url_id, tag);
 alter table chat_url_tag add constraint foreign key (chat_url_id) references chat_url (chat_url_id) ON DELETE CASCADE;
+
