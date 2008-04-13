9a259972e323e243f7754a05c756422ea7430ff0
alvin <alvinabad@users.noreply.github.com>
2008-04-13 23:29:15 +0000

    removed status=no

--- yaploud.com/js/util.js
+++ yaploud.com/js/util.js
@@ -41,7 +41,7 @@ function openChatWindow(site_url, title) {
     var url = "/chat/chat_window.php?url=" + site_url + "&title=" + title;
     
     var features = "width=340, height=340, status=yes, " +
-                   "scrollbars=1, menubar=no, toolbar=no, status=no, " +
+                   "scrollbars=1, menubar=no, toolbar=no, " +
                    "location=no, resizable=yes, left=100, top=100";
     window.open(url, "", features);
 }
