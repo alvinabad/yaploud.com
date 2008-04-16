349cfe6d23f82e7f82f3623328bf5c793831d260
alvin <alvinabad@users.noreply.github.com>
2008-04-16 11:34:53 +0000

    Fixed login, signup, and myaccount pages

---
+++ yaploud.com/user/login.php
@@ -0,0 +1,114 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<?php
+require("./login_c.inc");
+
+/*
+ * This is the view (MVC pattern) of the login page
+ * The controller is located in register_c.inc
+ * 
+ * PHP code in this view is only used for rendering logic and is kept 
+ * to the minimum. Please put all logic in the controller.
+ * 
+ * Created on Nov 18, 2007
+ * Author: alvinabad@alumni.cmu.edu
+ */
+?>
+
+<html>
+<head>
+  <title>YapLoud - Login</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <link type="text/css" rel="stylesheet" href="/css/style.css" />
+  <?php require("common/yui.php"); ?>
+    
+  <script type="text/javascript" src="/css/niftycube.js" ></script>
+  <script type="text/javascript">
+    YAHOO.util.Event.onDOMReady(function() {document.getElementById('username').focus();});
+  </script>
+</head>
+
+<body class="yui-skin-sam">
+<div id="container">
+<?php include("common/header1.php"); ?>
+<?php include("rightNav.php"); ?>
+
+<div id="content">
+
+  <div style="width: 600px; overflow: hidden;">
+  <div id="greeting">
+  <h1>Login at YapLoud</h1>
+    <h4>What is YapLoud?</h4>
+    YapLoud is the home for yapping about everything:
+    <ul style="margin-left: 0;">
+    <li style="margin-left: 0;">Chat about any topic of your interest
+    <li>Share favorite Yaps
+    <li>Connect with other yappers 
+    </ul>
+
+    <a href="/user/register.php">Sign up</a> now to join the YapLoud Community
+    <br>
+    <a href="/extension/yaploud.xpi" onclick="installxpi(this); return false;">Install</a> Firefox add-on
+  </div>
+
+  <div id="register">
+    <div style="color: red;" id="error_message">
+    <?php
+        if (!$result) {
+            print "$message <br>";
+        }
+    ?>
+    </div>
+    
+    <form action="/user/login.php" method="post" >
+          
+    <table style="text-align: left;">
+    <tr>
+      <td>
+        Username:
+        <br>
+        <input id="username" type="text" id="username" name="username" value="<?php print $username; ?>"/>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Password:
+        <br>
+        <input type="password" id="password" name="password" value=""/>
+      </td>
+    </tr>
+    <tr>
+      <td>
+      <input style="width:20px;" type="checkbox" name="remember" id=password >
+      Remember me
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input style="width:100px;" type="submit" value="Login" ;/>
+      </td>
+    </tr>
+    <tr>
+      <td>
+      <ul>
+      <li><a href="/user/forgotusername.php">Forgot username?</a>
+      <li><a href="/user/forgotpassword.php">Forgot password?</a>
+      <li><a href="/user/register.php">Not yet registered?</a>
+      </ul>
+      </td>
+    </tr>
+    </table>
+    
+    <input type=hidden name=action value=login />
+    
+    </form>
+    </div>
+  </div>
+
+</div> <!-- content -->
+
+<?php include("common/footer1.php"); ?>
+</div> <!-- container -->
+
+</body>
+</html>
---
+++ yaploud.com/user/myaccount_old.php
@@ -0,0 +1,123 @@
+<?php 
+include("./myaccount_c.inc");
+
+/*
+ * This is the view (MVC pattern) of My Account page.
+ * The controller is located at myaccount_c.inc
+ * 
+ * PHP code in this view is only used for rendering logic and is kept 
+ * to the minimum. Please put all logic in the controller.
+ * 
+ * Created on Nov 17, 2007
+ * Author: alvinabad@alumni.cmu.edu
+ * Revised:
+ */
+ 
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.yaploud.com/TR/html4/loose.dtd">
+
+<html>
+<head>
+<title>Yaploud - My Account</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="/css/chat.css" rel="stylesheet" type="text/css">
+<link href="/style.css" rel="stylesheet" type="text/css">
+<link href="/images/style_Yaploud.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="/user/ChangePassword.js" ></script>
+<script type="text/javascript" src="/user/UpdateUserInfo.js" ></script>
+
+</head>
+
+<body onLoad="ChangePassword.focus('password'); ChangePassword.clear();">
+
+<?php include("common/header2.php"); ?>
+
+<div style="float:left;width:700px;">
+  <div class=yap_title>My Account</div>
+  <p>
+
+  <div id="myaccount_change_password" style="float:left;width:350px;">
+    <form action="/user/myaccount.php" method="post" 
+          onSubmit="return ChangePassword.validate();">
+    <strong>Change Password</strong>
+    <p>
+    <div style="color: red;" id="error_message">
+       <?php
+           if ( isset($change_password_processed) && 
+                   $change_password_processed ) {
+               if ( $reset_password ) {
+                   print "Success! Your password has been changed. <br>";
+               }
+               else {
+                   print "Server error! Your password cannot be changed at " .
+                         "this time. Please try again later. <br>";
+               }
+           }
+       ?>
+    </div>
+    
+    <label for=yourName>Password: </label>
+    <br>
+    <input type="password" id="password" name="password" />
+    <br>
+    <label>Re-type password: </label>
+    <br>
+    <input type="password" id="password2" name="password2" />
+    <br>
+    <input type="submit" name="submit_change_password" value="Change password" />
+    </form>
+  </div>
+
+<div id="myaccount_updateUserInfo" style="float:left;width:350px;">
+    <form action="/user/myaccount.php" method="post" 
+          onSubmit="return UpdateUserInfo.validate();">
+    <strong>Update User Profile</strong>
+    <p>
+    <div style="color: red;" id="error_message_update_userinfo">
+       <?php
+           if ( isset($update_userinfo_processed) && 
+                   $update_userinfo_processed ) {
+               if ( $update_userinfo ) {
+                   print "Success! Your user information has been updated. <br>";
+               }
+               else {
+                   print "Server error! Your user information cannot be updated at " .
+                         "this time. Please try again later. <br>";
+               }
+           }
+       ?>
+    </div>
+    
+    <label for=yourName>Username: </label>
+    <strong><?php print $username; ?></strong>
+    <br>
+    <br>
+    <label>First Name: </label>
+    <br>
+    <input type="first_name" id="first_name" name="first_name" 
+           value="<?php print $first_name; ?>" />
+    <br>
+    <label>Last Name: </label>
+    <br>
+    <input type="last_name" id="last_name" name="last_name"
+           value="<?php print $last_name; ?>" />
+    <br>
+    <label>Email: </label>
+    <br>
+    <input type="email" id="email" name="email"
+           value="<?php print $email; ?>" />
+    <br>
+    <input type="submit" name="submit_update_userinfo" value="Update" />
+    </form>
+  </div>
+</div>
+
+<p>
+<div style="clear:both;"></div>
+<?php include("common/footer.php");?>
+
+</body>
+</html>
