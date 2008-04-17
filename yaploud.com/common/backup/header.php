3b7621d23b3866cd3a07e3992c2b048e80a32f6a
alvin <alvinabad@users.noreply.github.com>
2008-04-17 23:31:35 +0000

    Moved dead code to Archives

--- yaploud.com/common/backup/header.php
+++ yaploud.com/common/backup/header.php
@@ -3,9 +3,6 @@
   $user = new User();
 ?>
 
-
-<link href="style.css" rel="stylesheet" type="text/css"/>
-
 <?PHP
   // If the user pressed the logout link, end the session
   if (isset($_REQUEST['logout'])) {
@@ -13,56 +10,66 @@
   }
 ?>
 
-<!-- Top nav -->
-<table width="777" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-    <th width="777" align="left" valign="top" scope="col">
-      <table id="topNav" width="99%" cellspacing="0" cellpadding="0">
-	<tr>
-        <td width="163" valign="top" height="90" align="left" scope="row">
-			<img width="163" height="90" src="images/logo.gif"/>
-        <td width="43" valign="top" align="left">
-			<img width="43" height="90" src="images/home_02.gif"/>
-		</td>
-        <td width="328" valign="middle" align="left" class="top_imge"></td>
-        <td width="250" valign="top" align="left" class="top_imge">
-			<table width="100%" cellspacing="0" cellpadding="2" border="0">
-				<tbody>
-					<tr>
-					  <th scope="col" class="menu_1">
-					    <div align="center">
-					      <?PHP
-					        if (!isset($_SESSION['logged']) || $_SESSION['logged'] == false) {
-						  echo("<a scope=\"col\" class=\"menu_1\" href=\"http://www.yaploud.com/register.htm\"><strong>Sign Up</strong></a> | <a scope=\"col\" class=\"menu_1\" href=\"help.php\">Help</a> | <a scope=\"col\" class=\"menu_1\" href=login_page.php>Log In</a>");
-					        } else {
-					          $current_user = $_SESSION['username'];
-					          echo("Welcome $current_user | <a scope=\"col\" class=\"menu_1\" href=\"#\">My Account</a> | <a scope=\"col\" class=\"menu_1\" href=\"help.php\">Help</a> | <a scope=\"col\" class=\"menu_1\" href=\"http://www.yaploud.com/login_page.php?logout=true\">Log out</a>");
 
-					        }
-				  	      ?>
-					    </div>
-					  </th>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>
-							<table width="100%"  border="0" cellspacing="0" cellpadding="0">
-								<tr>
-									<th width="31%" class="Text2_b" scope="col">Search :</th>
-									<th width="44%" align="left" valign="top" scope="col">
-									<form method=get action=search.php><input name="q" type="text" class="Text2_b" size="16"></th>
-									<th width="25%" align="left" valign="top" scope="col"><div align="left"><a href="#"><img src="images/go_image.gif" width="29" height="21" border="0"></a></div></th>
-								</tr>
-							</table>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</td>
-        </tr>
-      </table>
-    </th>
-  </tr>
-</table>
+<script type="text/javascript">
+function isSearchValid() {
+    var txt = document.search_form.q.value;
+    txt = txt.replace(/^\s\s*/, '').replace(/\s\s*$/, '');  
+    document.search_form.q.value = txt;
+            
+    if (txt == "" ) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+        
+function submitSearch() {
+    if (isSearchValid()) {
+        document.search_form.submit();
+    }
+}
+</script>
+
+<!-- Top nav -->
+<div style="margin-bottom:5px;margin-top:10px; width:700px;">
+	<a href="/home.php"><img style="float:left;" alt="yaploud logo" width="163" height="90" border="0" src="images/logo.gif"></img></a>
+	<img style="float:left;" width="43" height="90" src="images/home_02.gif"></img>
+	<img style="float:left;" width=220 height=90 src="images/home_03.gif"></img>
+	<div style="float:left; padding-right:10px; height:90px; background-image: url(images/home_03.gif);">
+	<?
+	        if(!isset($_SESSION['logged']) || $_SESSION['logged'] == false) {
+			echo("<a scope=\"col\" class=\"menu_1\" href=\"/user/register.php\"><strong>Sign Up</strong></a> | <a scope=\"col\" class=\"menu_1\" href=\"help.php\">Feedback</a> | <a scope=\"col\" class=\"menu_1\" href=\"/login_page.php\">Log In</a>");
+		}
+		else {
+		    $current_user = $_SESSION['username'];
+		    echo("<a scope=\"col\" class=\"menu_1\" ><strong> Hi $current_user</strong></a> | <a scope=\"col\" class=\"menu_1\" href=\"/user/myaccount.php\">My Account</a> | <a scope=\"col\" class=\"menu_1\" href=\"help.php\">Feedback</a> | <a scope=\"col\" class=\"menu_1\" href=/login_page.php?logout=true>Log Out</a>");
+
+		    //echo("<span class=\"menu_1\"> <strong>Welcome, $current_user</strong></span> | <a scope=\"col\" class=\"menu_1\" href=\"myaccount.php\">My Account</a> | <a scope=\"col\" class=\"menu_1\" href=\"help.php\">Help</a> | <a scope=\"col\" class=\"menu_1\" href=\"/login_page.php?logout=true\">Log out</a>\n");
+	        }
+	?>
+		<form name="search_form" method=get action=search.php onsubmit="return isSearchValid();">
+		   <span class=menu_1 style="margin-top:5px; margin-left: 0px;">Search:</span>
+		   <input name="q" id=search_box type="text" class="Text2_b" size="16"></input>
+		   <a href="javascript: submitSearch();">
+		   <img src="images/go_image.gif" width="29" height="21" border="0"></img>
+		   </a>
+		</form>
+	</div>
+</div>
+<div style="clear:both;"></div>
+<table width="777"  border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <th width="777" align="left" valign="top" scope="col"><table width="99%"  border="0" cellspacing="0" cellpadding="0">
+
+      <tr>
+        <th align="left" valign="top" scope="row" width="163"><img src="images/home_06.gif" width="163" height="22"></th>
+        <td align="left" valign="top"><img src="images/home_07.gif" width="43" height="22">
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+  <tr>
+</table> <br>
+
+<!-- End Top Nav -->
--- yaploud.com/common/backup/header2.php
+++ yaploud.com/common/backup/header2.php
@@ -1,38 +1,89 @@
-<?PHP
-  include("user.php");
-  $user = new User();
-?>
 
+<script type="text/javascript">
+function strip_http() {
+    var url = document.getElementById("yapurl_box");
+    url.value = url.value.replace('http://', '');
+}
 
-<link href="style.css" rel="stylesheet" type="text/css"/>
-
-<?PHP
-  // If the user pressed the logout link, end the session
-  if (isset($_REQUEST['logout'])) {
-    $user->_logout();
-  }
-?>
+function isSearchValid() {
+    var txt = document.search_form.q.value;
+    txt = txt.replace(/^\s\s*/, '').replace(/\s\s*$/, '');  
+    document.search_form.q.value = txt;
+            
+    if (txt == "" ) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+        
+function submitSearch() {
+    if (isSearchValid()) {
+        document.search_form.submit();
+    }
+}
+</script>
 
 <!-- Top nav -->
-<div style="margin-top:20px; padding-top:20px;">
-	<img style="float:left;" alt="yaploud logo" width="163" height="90" src="images/logo.gif"></img>
-	<img style="float:left;" width="43" height="90" src="images/home_02.gif"></img>
-	<img style="float:left;" width=349 height=90 src="images/home_03.gif"></img>
-	<div style="float:left; height:90px; background:white;">
-	<?
-	        if(!isset($_SESSION['logged']) || $_SESSION['logged'] == false) {
-			echo("<a scope=\"col\" class=\"menu_1\" href=\"http://www.yaploud.com/register.htm\"><strong>Sign Up</strong></a> | <a scope=\"col\" class=\"menu_1\" href=\"help.php\">Help</a> | <a scope=\"col\" class=\"menu_1\" href=login_page.php>Log In</a>");
-		} 
-		else {
-		    $current_user = $_SESSION['username'];
-		    echo("<span class=menu_1>Welcome, $current_user</span> | <a scope=\"col\" class=\"menu_1\" href=\"#\">My Account</a> | <a scope=\"col\" class=\"menu_1\" href=\"help.php\">Help</a> | <a scope=\"col\" class=\"menu_1\" href=\"http://www.yaploud.com/login_page.php?logout=true\">Log out</a>");
-	        }
-	?>
-	<form method=get action=search.php>
-	<label class=menu_1 for=q>Search:</label>
-	<input name="q" id=search_box type="text" class="Text2_b" size="16">
-	<img src="images/go_image.gif" width="29" height="21" border="0">
-	</form>
-	</div>
-	<div style="clear:both;"></div>
+<div style="margin-bottom:5px;margin-top:10px; width:700px;">
+	<a href="/home.php"><img style="float:left;" alt="yaploud logo" width="163" 
+	   height="90" border="0" src="/images/logo.gif"></img></a>
+	<img style="float:left;" width="43" height="90" 
+	     src="/images/home_02.gif"></img>
+	<img style="float:left;" width=220 height=90 
+	     src="/images/home_03.gif"></img>
+	<div style="float:left; padding-right:10px; height:90px; 
+	     background-image: url(/images/home_03.gif);">
+	
+<?php
+    if(!isset($_SESSION['logged']) || $_SESSION['logged'] == false) {
+        echo <<<HTML
+    <a scope="col" class="menu_1" href="/user/register.php"><strong>Sign Up</strong></a> | 
+	<a scope="col" class="menu_1" href="/help.php">Feedback</a> | 
+	<a scope="col" class="menu_1" href="/login_page.php">Log In</a>
+HTML;
+    }
+	else {
+	    $current_user = $_SESSION['username'];
+		echo <<<HTML
+	<a scope="col" class="menu_1"><strong> Hi $current_user</strong></a> | 
+	<a scope="col" class="menu_1" href="/user/myaccount.php">My Account</a> | 
+	<a scope="col" class="menu_1" href="/help.php">Feedback</a> | 
+	<a scope="col" class="menu_1" href=/login_page.php?logout=true>Log Out</a>
+HTML;
+
+    }
+?>
+
+    <form name="search_form" method=get action="/search.php" onsubmit="return isSearchValid();">
+    <span class=menu_1 style="margin-top:5px; margin-left: 0px;">Search:</span>
+    <input name="q" id=search_box type="text" class="Text2_b" size="16"></input>
+    <a href="javascript: submitSearch();">
+    <img src="/images/go_image.gif" width="29" height="21" border="0"></img>
+    </a>
+    </form>
+    </div>
 </div>
+
+<div style="clear:both;"></div>
+<table width="777"  border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <th width="100%" align="left" valign="top" scope="col">
+    <table width="99%"  
+        border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <th align="left" valign="top" scope="row" width="163"><img alt="home_06" 
+            src="/images/home_06.gif" width="163" height="22"></th>
+        <td align="left" valign="top"><img alt="home_07" 
+            src="/images/home_07.gif" width="43" height="22"></td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+  <tr>
+</table>
+
+<br>
+
+<!-- End Top Nav -->
