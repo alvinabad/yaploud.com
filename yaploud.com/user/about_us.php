a46976461082a096c90f6d9d811199dce69572cf
yaploud <yaploud@5c49d15a-261d-0410-96dc-941ab20fcdd5>
2008-04-25 05:09:49 +0000

    Added about us page

---
+++ yaploud.com/user/about_us.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<?php
+    require("./home_c.inc");
+?>
+
+<html>
+    <head>
+        <title>About YapLoud</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <link type="text/css" rel="stylesheet" href="/css/style.css" />
+    </head>
+    
+    <body class="yui-skin-sam">
+        <div id="container">
+            <?php include("common/header1.php"); ?>
+            <?php include("rightNav.php"); ?>
+            
+            <div id="content">
+                <label>About YapLoud</label><br/>
+                YapLoud is a FREE service that allows web users to connect with like-minded people on the fly.
+                Today, as users are surfing through the web, they are unable to interact with the web content. 
+                There is no way for people to talk about its content either with the website owner or with other surfers.
+                YapLoud enables this. Users can no longer be passive readers of the content. Instead, they can identify
+                what other Yappers are talking about and participate in the Yapper Community. They can also participate
+                with the website owners directly.
+                
+                YapLoud empowers surfers to influence the content on any website.
+                <br/><hr/>
+                
+                <label>History of YapLoud</label><br/>
+                YapLoud idea started off with the notion of getting new parents to be able to easily connect with each other.
+                The idea took incubation in Carnegie Mellon West, Mountain View, and is still its headquarters.
+                Our team includes CMU West Students and Faculty.
+                <br/><hr/>
+                
+                <label>What people are saying about YapLoud</label><br/>
+                <ul>
+                    <li>The thing that is great about this idea is that it simply overlays the entire web rather than creating a walled garden” – Dean of top university</li>
+                    <li>“Where can I download this application?” – Sr Product manager, WebEx</li>
+                    <li>"Can you do this? This will be great for us” – Director of a Fortune 1000 company</li>
+                </ul>
+                
+            </div> <!-- content -->
+            
+            <?php include("common/footer1.php"); ?>
+        </div> <!-- container -->
+        
+        <?php require("common/yui.php"); ?>
+        <script type="text/javascript" src="/css/niftycube.js" ></script>
+        <script type="text/javascript" src="/js/home.js" ></script>
+    </body>
+</html>
+
