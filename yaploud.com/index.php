668cf04c15aa96c567d87773823307da101e131e
alvin <alvinabad@users.noreply.github.com>
2008-05-05 04:23:48 +0000

    Added redirect to home.php

--- yaploud.com/index.php
+++ yaploud.com/index.php
@@ -1,3 +1,10 @@
+<?php
+
+$redirect = '/home.php';
+header("Location: $redirect");
+
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
