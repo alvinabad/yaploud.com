06ec7f72ced3df2de7905d08a2b770299f0b97a9
alvin <alvinabad@users.noreply.github.com>
2008-05-07 04:18:04 +0000

    Fixed chat window overlapping

--- yaploud.com/extension/yaploud/chrome/content/yaploud.js
+++ yaploud.com/extension/yaploud/chrome/content/yaploud.js
@@ -207,7 +207,8 @@ function openChatWindow(site_url, title, description) {
     
     var features = "width=320, height=320, status=yes, " +
                    "menubar=no, toolbar=no, status=no, " +
-                   "location=no, resizable=yes, left=100, top=100";
+                   "location=no, resizable=yes ";
+                   //left=100, top=100";
     window.open(url, "", features);
 }
 
