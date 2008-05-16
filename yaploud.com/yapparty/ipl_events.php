8d90aa7e4d16aaf690e57d48df6fd17e156cde6d
alvin <alvinabad@users.noreply.github.com>
2008-05-16 04:19:16 +0000

    Added ipl_events.php

---
+++ yaploud.com/yapparty/ipl_events.php
@@ -0,0 +1,215 @@
+<?php
+set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT']);
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <title>YapLoud Party</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <link type="text/css" rel="stylesheet" href="/css/style.css" />
+</head>
+
+<body class="yui-skin-sam">
+<div id="container">
+<?php include("common/header1.php"); ?>
+<?php include("rightNav.php"); ?>
+
+<div id="content">
+
+                <!--<div id="ipl_logo" align="right"><img src="header-sub-logo-blue-page.png" /></div>-->
+                
+                <p>
+                    <img align="left" width="220px" height="180px" src="IPL_Logo.png" />
+                    <img align="right" width="120px" height="120px" src="header-sub-logo-blue-page.png" />
+                    <h1>Come join the online IPL Yap Parties</h1>
+                    <!--
+                    <div><strong>Where is the party?</strong> <a onclick="openChatWindow('http://www.themumbaiindians.com', 'http://www.themumbaiindians.com');" href="http://www.themumbaiindians.com">Click here</a></div>
+                    <div><strong>When is the party?</strong> Sunday 18th 7:30AM PST. Also Live during any IPL matches.</div>
+                    <div><strong>Theme of the party?</strong> Mumbai Indians vs Deccan Chargers</div>
+                    -->
+                    <div>
+                    <ul>
+                        <li>No plugin or installation required</li>
+                        <li>No Registration required*</li>
+                    </ul>
+                    </div>
+                
+                <div>
+                    Immediately get connected with your friends and Yap live while the match is going on.
+                </div>
+                <div style="font-size:10px">* Randomly selected registered users are eligiable to receive a surprise YapLoud gift.</div>
+                
+                <hr/>
+                <div>
+                    <table border="1px" align="left" style="width: 400px">
+                        <tbody style="font-size:12px">
+                            <tr>
+                                <td class="Teamheadtxt" width="25%">Date</td>
+                                <td class="Teamheadtxt" style="border-left: 3px solid rgb(255, 255, 255); border-right: 3px solid rgb(255, 255, 255);" >Team v Team</td>
+                                <!--
+                                <td style="border-left: 3px solid rgb(255, 255, 255); border-right: 3px solid rgb(255, 255, 255);" class="Teamheadtxt" width="30%">Team v Team</td>
+                                <td class="Teamheadtxt" width="50%">Venue</td>
+                                -->
+                            </tr>
+                            <!--
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Wed 14<br>20:00 local, 14:30 GMT</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">36th match - Mumbai Indians v Chennai Super Kings</td>
+                                <td class="blk-txt2" bgcolor="#f5f5f5">Wankhede Stadium, Mumbai</td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Thu 15 <br>20:00 local, 14:30 GMT</td>
+                                <td class="teamtdborder">37th match - Delhi Daredevils v Deccan Chargers</td>
+                                <td class="blk-txt2">Feroz Shah Kotla, Delhi</td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Fri 16<br>20:00 local, 14:30 GMT</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">38th match - Mumbai Indians v Kolkata Knight Riders</td>
+                                <td class="blk-txt2" bgcolor="#f5f5f5">Wankhede Stadium, Mumbai</td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Sat 17<br>16:00 local, 10:30 GMT</td>
+                                <td class="teamtdborder">39th match - Rajasthan Royals v Bangalore Royal Challengers</td>
+                                <td class="blk-txt2">Sawai Mansingh Stadium, Jaipur</td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Sat 17 <br>20:00 local, 14:30 GMT</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">40th match - Delhi Daredevils v Kings XI Punjab</td>
+                                <td class="blk-txt2" bgcolor="#f5f5f5">Feroz Shah Kotla, Delhi</td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Sun 18<br>16:00 local, 10:30 GMT</td>
+                                <td class="teamtdborder">41st match - Kolkata Knight Riders v Chennai Super Kings</td>
+                                <td class="blk-txt2">Eden Gardens, Kolkata</td>
+                            </tr>
+                            -->
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Sun 18 <br/>07:30AM PST</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">42nd match - <a href="http://www.deccanchargers.com/" onclick='openChatWindow("http://www.deccanchargers.com/", "http://www.deccanchargers.com/");'>Deccan Chargers</a> <br/>v <a href="http://www.themumbaiindians.com/" onclick='openChatWindow("http://www.themumbaiindians.com/", "http://www.themumbaiindians.com/");'>Mumbai Indians</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Mon 19 <br/>07:30AM PST</td>
+                                <td class="teamtdborder">43rd match - <a href="http://www.royalchallengers.com/" onclick='openChatWindow("http://www.royalchallengers.com/", "http://www.royalchallengers.com/");'>Bangalore Royal Challengers</a> <br/>v <a href="http://www.delhidaredevils.com/" onclick='openChatWindow("http://www.delhidaredevils.com/", "http://www.delhidaredevils.com/");'>Delhi Daredevils</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Tue 20 <br/>07:30AM PST</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">44th match - <a href="http://www.kolkataknightriders.com/" onclick='openChatWindow("http://www.kolkataknightriders.com/", "http://www.kolkataknightriders.com/");'>Kolkata Knight Riders</a> <br/>v <a href="http://www.rajasthanroyals.com/" onclick='openChatWindow("http://www.rajasthanroyals.com/", "http://www.rajasthanroyals.com/");'>Rajasthan Royals</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Wed 21 <br/>03:30AM PST</td>
+                                <td class="teamtdborder">45th match - <a href="http://www.themumbaiindians.com/" onclick='openChatWindow("http://www.themumbaiindians.com/", "http://www.themumbaiindians.com/");'>Mumbai Indians</a> <br/>v <a href="http://www.kingsxipunjab.com/" onclick='openChatWindow("http://www.kingsxipunjab.com/", "http://www.kingsxipunjab.com/");'>Kings XI Punjab</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Wed 21 <br/>07:30AM PST</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">46th match - <a href="http://www.chennaisuperkings.com/" onclick='openChatWindow("http://www.chennaisuperkings.com/", "http://www.chennaisuperkings.com/");'>Chennai Super Kings</a> <br/>v <a href="http://www.royalchallengers.com/" onclick='openChatWindow("http://www.royalchallengers.com/", "http://www.royalchallengers.com/");'>Bangalore Royal Challengers</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Thu 22 <br/>07:30AM PST</td>
+                                <td class="teamtdborder">47th match - <a href="http://www.delhidaredevils.com/" onclick='openChatWindow("http://www.delhidaredevils.com/", "http://www.delhidaredevils.com/");'>Delhi Daredevils</a> <br/>v <a href="http://www.kolkataknightriders.com/" onclick='openChatWindow("http://www.kolkataknightriders.com/", "http://www.kolkataknightriders.com/");'>Kolkata Knight Riders</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Fri 23 <br/>07:30AM PST</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">48th match - <a href="http://www.kingsxipunjab.com/" onclick='openChatWindow("http://www.kingsxipunjab.com/", "http://www.kingsxipunjab.com/");'>Kings XI Punjab</a> <br/>v <a href="http://www.deccanchargers.com/" onclick='openChatWindow("http://www.deccanchargers.com/", "http://www.deccanchargers.com/");'>Deccan Chargers</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Sat 24 <br/>03:30AM PST</td>
+                                <td class="teamtdborder">49th match - <a href="http://www.chennaisuperkings.com/" onclick='openChatWindow("http://www.chennaisuperkings.com/", "http://www.chennaisuperkings.com/");'>Chennai Super Kings</a> <br/>v <a href="http://www.rajasthanroyals.com/" onclick='openChatWindow("http://www.rajasthanroyals.com/", "http://www.rajasthanroyals.com/");'>Rajasthan Royals</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Sat 24 <br/>07:30AM PST</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">50th match - <a href="http://www.delhidaredevils.com/" onclick='openChatWindow("http://www.delhidaredevils.com/", "http://www.delhidaredevils.com/");'>Delhi Daredevils</a> <br/>v <a href="http://www.themumbaiindians.com/" onclick='openChatWindow("http://www.themumbaiindians.com/", "http://www.themumbaiindians.com/");'>Mumbai Indians</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Sun 25 <br/>03:30AM PST</td>
+                                <td class="teamtdborder">52nd match - <a href="http://www.deccanchargers.com/" onclick='openChatWindow("http://www.deccanchargers.com/", "http://www.deccanchargers.com/");'>Deccan Chargers</a> <br/>v <a href="http://www.royalchallengers.com/" onclick='openChatWindow("http://www.royalchallengers.com/", "http://www.royalchallengers.com/");'>Bangalore Royal Challengers</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Sun 25 <br/>07:30AM PST</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">51st match - <a href="http://www.kolkataknightriders.com/" onclick='openChatWindow("http://www.kolkataknightriders.com/", "http://www.kolkataknightriders.com/");'>Kolkata Knight Riders</a> <br/>v <a href="http://www.kingsxipunjab.com/" onclick='openChatWindow("http://www.kingsxipunjab.com/", "http://www.kingsxipunjab.com/");'>Kings XI Punjab</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Mon 26 <br/>07:30AM PST</td>
+                                <td class="teamtdborder">53rd match - <a href="http://www.rajasthanroyals.com/" onclick='openChatWindow("http://www.rajasthanroyals.com/", "http://www.rajasthanroyals.com/");'>Rajasthan Royals</a> <br/>v <a href="http://www.themumbaiindians.com/" onclick='openChatWindow("http://www.themumbaiindians.com/", "http://www.themumbaiindians.com/");'>Mumbai Indians</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Tue 27 <br/>07:30AM PST</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">54th match - <a href="http://www.deccanchargers.com/" onclick='openChatWindow("http://www.deccanchargers.com/", "http://www.deccanchargers.com/");'>Deccan Chargers</a> <br/>v <a href="http://www.chennaisuperkings.com/" onclick='openChatWindow("http://www.chennaisuperkings.com/", "http://www.chennaisuperkings.com/");'>Chennai Super Kings</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">May, Wed 28 <br/>03:30AM PST</td>
+                                <td class="teamtdborder">55th match - <a href="http://www.royalchallengers.com/" onclick='openChatWindow("http://www.royalchallengers.com/", "http://www.royalchallengers.com/");'>Bangalore Royal Challengers</a> <br/>v <a href="http://www.themumbaiindians.com/" onclick='openChatWindow("http://www.themumbaiindians.com/", "http://www.themumbaiindians.com/");'>Mumbai Indians</a></td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Wed 28 <br/>07:30AM PST</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">56th match - <a href="http://www.kingsxipunjab.com/" onclick='openChatWindow("http://www.kingsxipunjab.com/", "http://www.kingsxipunjab.com/");'>Kings XI Punjab</a> <br/>v <a href="http://www.rajasthanroyals.com/" onclick='openChatWindow("http://www.rajasthanroyals.com/", "http://www.rajasthanroyals.com/");'>Rajasthan Royals</a></td>
+                            </tr>
+                            <!--
+                            <tr height="40">
+                                <td class="blk-txt2">May, Fri 30<br>07:30AM PST</td>
+                                <td class="teamtdborder">1st Semi-Final - TBC v TBC</td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2" bgcolor="#f5f5f5">May, Sat 31<br>07:30AM PST</td>
+                                <td class="teamtdborder" bgcolor="#f5f5f5">2nd Semi-Final - TBC v TBC</td>
+                            </tr>
+                            <tr height="40">
+                                <td class="blk-txt2">June, Sun 1<br>07:30AM PST</td>
+                                <td class="teamtdborder">Final - TBC v TBC</td>
+                            </tr>
+                            -->
+                        </tbody>
+                    </table>
+                    <div align="center">
+                    <strong>Check out what people are yapping about Mumbai Indians</strong>
+                    <p>
+                    <iframe src="http://www.yaploud.com/chat/chat_window.php?url=http://www.themumbaiindians.com&title=http://www.themumbaiindians.com" 
+                            scrolling="no" style="width:325px; height:320px" frameborder="1"></iframe>
+                    <br>
+                    <a href="http://www.themumbaiindians.com" 
+                       onclick='openChatWindow("http://www.themumbaiindians.com", "http://www.themumbaiindians.com");'>
+                       Chat on a separate window</a>
+                    </div>
+                </div>
+                        
+                <!--
+                <p>
+                <div>
+                    <strong>New to YapLoud?</strong>
+                    <p>
+                        YapLoud dynamically connects all users browsing a website! This allows like-minded users to start interacting with each other in real-time.
+                        Check out what yappers are yapping about <a onclick="openChatWindow('http://www.themumbaiindians.com', 'http://www.themumbaiindians.com');" href="http://www.themumbaiindians.com">Mumbai Indians.</a>
+                    </p>
+                </div>
+                </p>
+                
+                <hr/>
+                <p>
+                    <strong>Want to create your own Yap Party?</strong>
+                    <p>
+                        Any user can create a Yap Party.
+                        <ul>
+                            <li>Pick a website (URL)</li>
+                            <li>Pick a time</li>
+                            <li>Notify your friends to come to that URL at the time</li>
+                        </ul>
+                        All users would automatically get connected and they can start sharing conversations with each other.
+                        <div>
+                            We can also help you create your Yap Party. <a href="help.php"> Send us a note.</a>
+                        </div>
+                    </p>
+                </p>
+                -->
+            </div>
+            
+</div> <!-- content -->
+
+<?php include("common/footer1.php"); ?>
+</div> <!-- container -->
+
+  <?php require("common/yui.php"); ?>
+  <script type="text/javascript" src="/css/niftycube.js" ></script>
+  <script type="text/javascript" src="/js/home.js" ></script>
+  
+</body>
+</html>
