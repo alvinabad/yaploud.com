68921b551572912346946cab0b3b19189d4d726c
alvin <alvinabad@users.noreply.github.com>
2008-06-17 05:28:17 +0000

    Fixed generate_yaplet_link.js

--- yaploud.com/chat/generate_yaplet_link.js
+++ yaploud.com/chat/generate_yaplet_link.js
@@ -1,4 +1,31 @@
 
+function openChatWindow(site_url, title) {
+    if (site_url == null) {
+        site_url = ext_url;
+    }
+    
+    if (title == null) {
+        title = site_url;
+    }
+    
+    // check if attempting to chat same site
+    if (site_url.indexOf(location.hostname) != -1) {
+        return;
+    }
+    
+    // bypass Apache' mod_security: Access denied with code 503 
+    title = title.replace(/\s+\|/g, ' -').replace(/\|\s+/g, '- '); 
+    
+    site_url = encodeURIComponent_recursive(site_url);
+    title = encodeURIComponent_recursive(title);
+    var url = "/chat/chat_window.php?url=" + site_url + "&title=" + title;
+    
+    var features = "width=340, height=340, status=yes, " +
+                   "scrollbars=no, menubar=no, toolbar=no, " +
+                   "location=no, resizable=yes";
+                   //"location=no, resizable=yes, left=100, top=100";
+    window.open(url, "", features);
+}
 
 var yaploud_yaplink_el = document.getElementById('yaploud_yaplink');
 if (yaploud_yaplink_el && document.location) {
@@ -6,7 +33,7 @@ if (yaploud_yaplink_el && document.location) {
     var link_name = 'Open Yaplet';
     var code = '<a href="javascript: openChatWindow(' +
                "'" + url + "', " +
-               "'" + url + "') void 0;" + '"' +
+               "'" + url + "'); void 0;" + '"' +
                '>' + link_name + '</a>'; 
     alert(code);
     yaploud_yaplink_el.innerHTML = code;
