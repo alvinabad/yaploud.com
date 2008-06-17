9f1c33e27d721a09b23a783004049c907cad03c5
alvin <alvinabad@users.noreply.github.com>
2008-06-17 06:01:31 +0000

    Fixed generate_yaplet_link.js

--- yaploud.com/chat/generate_yaplet_link.js
+++ yaploud.com/chat/generate_yaplet_link.js
@@ -1,17 +1,25 @@
 
+function includeJavascript(src) {
+    if (document.createElement && document.getElementsByTagName) {
+        var head_tag = document.getElementsByTagName('head')[0];
+        var script_tag = document.createElement('script');
+        script_tag.setAttribute('type', 'text/javascript');
+        script_tag.setAttribute('src', src);
+        head_tag.appendChild(script_tag);
+    }
+}
+
+includeJavascript('http://www.yaploud.com/js/util.js');
+
 var yaploud_yaplink_el = document.getElementById('yaploud_yaplink');
 if (yaploud_yaplink_el && document.location) {
     var url = document.location;
     var link_name = 'Open Yaplet';
     var code = '<a href="javascript: ' +
-               "alert('yaplet')" +
-    /**
                'openChatWindow(' +
                "'" + url + "', " +
                "'" + url + "')" +
-     **/
                '; void 0;' + '"' +
                '>' + link_name + '</a>'; 
-    alert(code);
     yaploud_yaplink_el.innerHTML = code;
 }
