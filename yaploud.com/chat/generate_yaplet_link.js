a5d496b26e7857b64f4d113227ee405708f6e7ea
alvin <alvinabad@users.noreply.github.com>
2008-06-17 05:32:09 +0000

    Fixed generate_yaplet_link.js

--- yaploud.com/chat/generate_yaplet_link.js
+++ yaploud.com/chat/generate_yaplet_link.js
@@ -1,32 +1,4 @@
 
-function openChatWindow(site_url, title) {
-    if (site_url == null) {
-        site_url = ext_url;
-    }
-    
-    if (title == null) {
-        title = site_url;
-    }
-    
-    // check if attempting to chat same site
-    if (site_url.indexOf(location.hostname) != -1) {
-        return;
-    }
-    
-    // bypass Apache' mod_security: Access denied with code 503 
-    title = title.replace(/\s+\|/g, ' -').replace(/\|\s+/g, '- '); 
-    
-    site_url = encodeURIComponent_recursive(site_url);
-    title = encodeURIComponent_recursive(title);
-    var url = "/chat/chat_window.php?url=" + site_url + "&title=" + title;
-    
-    var features = "width=340, height=340, status=yes, " +
-                   "scrollbars=no, menubar=no, toolbar=no, " +
-                   "location=no, resizable=yes";
-                   //"location=no, resizable=yes, left=100, top=100";
-    window.open(url, "", features);
-}
-
 var yaploud_yaplink_el = document.getElementById('yaploud_yaplink');
 if (yaploud_yaplink_el && document.location) {
     var url = document.location;
