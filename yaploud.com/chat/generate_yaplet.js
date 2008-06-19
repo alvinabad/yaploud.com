41ae036250c67d92b430ad4b37d21e1e6030dddf
alvin <alvinabad@users.noreply.github.com>
2008-06-19 03:16:34 +0000

    Added id and class attributes for yaplet and yaplink

--- yaploud.com/chat/generate_yaplet.js
+++ yaploud.com/chat/generate_yaplet.js
@@ -1,12 +1,12 @@
 
 var yaploud_yaplet_el = document.getElementById('yaploud_yaplet');
 if (yaploud_yaplet_el && document.location) {
-	if (!url) {
-        var url = document.location;
+	if (!yaplet_url) {
+        var yaplet_url = document.location;
 	}
     var code = '<iframe id="yaplet_iframe_id" class="yaplet_iframe_class" src=' + 
-               '"http://www.yaploud.com/chat/chat_window.php?url=' + url +
-               '&title=' + url + '"' +
+               '"http://www.yaploud.com/chat/chat_window.php?url=' + yaplet_url +
+               '&title=' + yaplet_url + '"' +
                ' scrolling="no" style="width:325px; ' +
                'height:320px" frameborder="1"></iframe>';
     
--- yaploud.com/chat/generate_yaplet_link.js
+++ yaploud.com/chat/generate_yaplet_link.js
@@ -13,8 +13,8 @@ includeJavascript('http://www.yaploud.com/js/util.js');
 
 var yaploud_yaplink_el = document.getElementById('yaploud_yaplink');
 if (yaploud_yaplink_el && document.location) {
-	if (!url) {
-        var url = document.location;
+	if (!yaplink_url) {
+        var yaplink_url = document.location;
 	}
 	
 	if (!link_name) {
@@ -24,8 +24,8 @@ if (yaploud_yaplink_el && document.location) {
     var code = '<a id="yaplink_href_id" class="yaplink_href_class" ' +
                'href="javascript: ' +
                'openChatWindow(' +
-               "'" + url + "', " +
-               "'" + url + "')" +
+               "'" + yaplink_url + "', " +
+               "'" + yaplink_url + "')" +
                '; void 0;' + '"' +
                '>' + link_name + '</a>'; 
     yaploud_yaplink_el.innerHTML = code;
