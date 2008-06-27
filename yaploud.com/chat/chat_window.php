97304b38d8a5b3be070be27ed03e7d9d25e0f35e
alvin <alvinabad@users.noreply.github.com>
2008-06-27 06:32:12 +0000

    Converted to onclick link

--- yaploud.com/chat/chat_window.php
+++ yaploud.com/chat/chat_window.php
@@ -31,7 +31,7 @@ if(!$iframe) {
 ?>
   
   <div class="hd" id="hd">
-    <a href="javascript: openExternalWindow('/home.php'); void 0;">
+    <a href="/home.php" onclick="openExternalWindow('/home.php'); return false;">
       <img src=/images/logo.gif border="0" width=41 height=22 valign=absmiddle></img></a>
       <strong>Yapping about: </strong>
       <?php
