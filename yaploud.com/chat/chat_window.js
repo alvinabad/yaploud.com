676e055956faf0bf2f7e2da9fe5f8d37a2a96918
alvin <alvinabad@users.noreply.github.com>
2008-07-01 08:07:04 +0000

    increased yaplet height due to petchat

--- yaploud.com/chat/chat_window.js
+++ yaploud.com/chat/chat_window.js
@@ -936,7 +936,7 @@ function quit() {
 }
 
 function resizeDivs() {
-    var newHeight = 150 + YAHOO.util.Dom.getViewportHeight() - yaplet_height;
+    var newHeight = 150 + YAHOO.util.Dom.getViewportHeight() - 340;
     if (newHeight > 150) {
         YAHOO.util.Dom.setStyle($("msg"), 'height', newHeight + "px");
         YAHOO.util.Dom.setStyle($("yappers"), 'height', newHeight + "px");
