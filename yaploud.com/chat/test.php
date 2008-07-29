f489645a24d8cbf0a565ed2c2403f8316df416b6
asriar <asriar@5c49d15a-261d-0410-96dc-941ab20fcdd5>
2008-07-29 04:21:32 +0000

    added file to test dogtime yaplet

---
+++ yaploud.com/chat/test.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <title>Testing Dogtime Yaplink</title>
+  </head>
+<body>
+    
+<h1> Testing Dogtime Yaplink </h1>
+
+    <!-- START EMBEDDED YAPLINK CODE -->
+    <div id="yaploud_yaplink"></div>
+
+    <style>
+        // Optional: set css style for the yaplink
+        #yaplink_href_id {
+        }
+        .yaplink_href_class {
+            font-size: 22px;
+            color: brown;
+        }
+    </style>
+    <script type="text/javascript">
+        var yaplink_name = "PetChat";
+        var yaploud_client = "dogtime";
+    </script>
+    <script type="text/javascript" src="http://www.yaploud.com/chat/generate_yaplet_link.js"></script>
+    <!-- END EMBEDDED YAPLINK CODE -->
+
+
+</body>
+</html>
