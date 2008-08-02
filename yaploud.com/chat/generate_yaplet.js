3aa9732cc7dd4e069ffce73f884ece01ffa698ad
alvin <alvinabad@users.noreply.github.com>
2008-08-02 18:15:35 +0000

    Added yaploud_client

--- yaploud.com/chat/generate_yaplet.js
+++ yaploud.com/chat/generate_yaplet.js
@@ -4,9 +4,16 @@ if (yaploud_yaplet_el && document.location) {
 	if (!yaplet_url) {
         var yaplet_url = document.location;
 	}
+	
+	if (typeof(yaploud_client) == "undefined") {
+		yaploud_client = '';
+	}
+	
+	var yaploud_host = "www.yaploud.com";
+	
     var code = '<iframe id="yaplet_iframe_id" class="yaplet_iframe_class" src=' + 
-               '"http://www.yaploud.com/chat/chat_window.php?url=' + yaplet_url +
-               '&title=' + yaplet_url + '"' +
+               '"http://' + yaploud_host + '/chat/chat_window.php?url=' + yaplet_url +
+               '&title=' + yaplet_url + '&c=' + yaploud_client + '"' +
                ' scrolling="no" style="width:325px; ' +
                'height:320px" frameborder="1"></iframe>';
     
