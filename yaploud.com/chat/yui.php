294283d6f913bd0ab2a0afafa9865bdbbd1e044e
joshuacorrea <joshuacorrea@5c49d15a-261d-0410-96dc-941ab20fcdd5>
2008-10-05 23:19:04 +0000

    yui from yahoo servers

--- yaploud.com/chat/yui.php
+++ yaploud.com/chat/yui.php
@@ -1,31 +1,31 @@
 
-<link rel="stylesheet" type="text/css" href="/js/yui/build/button/assets/skins/sam/button.css" />
-<link rel="stylesheet" type="text/css" href="/js/yui/build/container/assets/skins/sam/container.css">
-<link rel="stylesheet" type="text/css" href="/js/yui/build/resize/assets/skins/sam/resize.css" />
-<link rel="stylesheet" type="text/css" href="/js/yui/build/tabview/assets/skins/sam/tabview.css">
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.2/build/button/assets/skins/sam/button.css" />
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.2/build/container/assets/skins/sam/container.css">
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.2/build/resize/assets/skins/sam/resize.css" />
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.2/build/tabview/assets/skins/sam/tabview.css">
 
-<script type="text/javascript" src="/js/yui/build/utilities/utilities.js"></script>
-<script type="text/javascript" src="/js/yui/build/button/button-min.js"></script>
-<script type="text/javascript" src="/js/yui/build/container/container-min.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/utilities/utilities.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/button/button-min.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/container/container-min.js"></script>
 
-<script type="text/javascript" src="/js/yui/build/yahoo-dom-event/yahoo-dom-event.js"></script>
-<script type="text/javascript" src="/js/yui/build/dragdrop/dragdrop-min.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/yahoo-dom-event/yahoo-dom-event.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/dragdrop/dragdrop-min.js"></script>
 
-<script type="text/javascript" src="/js/yui/build/yahoo/yahoo-min.js"></script> 
-<script type="text/javascript" src="/js/yui/build/event/event-min.js"></script> 
-<script type="text/javascript" src="/js/yui/build/connection/connection-min.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/yahoo/yahoo-min.js"></script> 
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/event/event-min.js"></script> 
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/connection/connection-min.js"></script>
 
 <!-- Utility Dependencies -->
-<script type="text/javascript" src="/js/yui/build/element/element-beta-min.js"></script> 
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/element/element-beta-min.js"></script> 
 <!-- Optional Animation -->
-<script type="text/javascript" src="/js/yui/build/animation/animation-min.js"></script> 
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/animation/animation-min.js"></script> 
 
 <!-- Source file for the Resize Utility -->
-<script type="text/javascript" src="/js/yui/build/resize/resize-beta.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/resize/resize-beta.js"></script>
 
 <!-- Source file for TabView -->
-<script type="text/javascript" src="/js/yui/build/tabview/tabview-min.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.5.2/build/tabview/tabview-min.js"></script>
 
 <!-- 
-<link rel="stylesheet" type="text/css" href="/js/yui/build/fonts/fonts-min.css" />
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.2/build/fonts/fonts-min.css" />
  -->
