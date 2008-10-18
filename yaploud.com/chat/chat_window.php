ffd267c33d3bc8e20a230401522d37743e75841f
joshuacorrea <joshuacorrea@5c49d15a-261d-0410-96dc-941ab20fcdd5>
2008-10-18 21:37:09 +0000

    added google analytics

--- yaploud.com/chat/chat_window.php
+++ yaploud.com/chat/chat_window.php
@@ -348,5 +348,6 @@ HTML;
 	}
 
 </script>
+<script type="text/javascript"> var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www."); document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E")); </script> <script type="text/javascript"> var pageTracker = _gat._getTracker("UA-5654854-1"); pageTracker._trackPageview(); </script>
 </body>
 </html>
