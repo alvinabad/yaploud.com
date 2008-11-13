e62024a303470c0ec25678f918830a9fcc4251f4
rarora <rarora@5c49d15a-261d-0410-96dc-941ab20fcdd5>
2008-11-13 06:17:58 +0000

    made php as first tag to get rid of small error

--- yaploud.com/user/forgotusername.php
+++ yaploud.com/user/forgotusername.php
@@ -1,16 +1,14 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/strict.dtd">
-
-<?php
-include("./forgotusername_c.inc");
+<?php include("./forgotusername_c.inc");
 
 /*
  * This is the view (MVC pattern) of the forgotusername page
  * The controller is located in forgotusername_c.inc
- * 
- * PHP code in this view is only used for rendering logic and is kept 
+ *
+ * PHP code in this view is only used for rendering logic and is kept
  * to the minimum. Please put all logic in the controller.
  */
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/strict.dtd">
 
 <html>
 <head>
@@ -18,7 +16,7 @@ include("./forgotusername_c.inc");
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
   <link type="text/css" rel="stylesheet" href="/css/style.css" />
   <?php require("common/yui.php"); ?>
-    
+
   <script type="text/javascript" src="/css/niftycube.js" ></script>
   <script type="text/javascript" src="/user/forgotusername.js" ></script>
   <script type="text/javascript">
@@ -39,7 +37,7 @@ include("./forgotusername_c.inc");
     <ul style="margin-left: 0;">
     <li style="margin-left: 0;">Enter the email address associated with your YapLoud account
     <li>Enter the verification code you see in the box
-    <li>We'll send you an email with your username 
+    <li>We'll send you an email with your username
     </ul>
 
     <a href="/user/register.php">Sign up</a> now to join the YapLoud Community
@@ -59,11 +57,11 @@ include("./forgotusername_c.inc");
               print "Your email is not found in our system. " .
                     "Please try another email. <p>";
           }
-      }       
+      }
     ?>
   </div>
 
-  <div id="content_2">    
+  <div id="content_2">
     <div id="error_message" style="color: red; font-weight: normal;">
     <?php
       if (isset($post_processed) && $post_processed &&
@@ -72,11 +70,11 @@ include("./forgotusername_c.inc");
       }
     ?>
     </div>
-    
+
     <form action="/user/forgotusername.php" method="post"
           name="forgotusername_form" id="forgotusername_form"
           onSubmit="return ForgotUsername.validate();">
-          
+
     <table style="text-align: left;">
     <tr>
       <td>
@@ -102,9 +100,9 @@ include("./forgotusername_c.inc");
       </td>
     </tr>
     </table>
-    
+
     <input type=hidden name=action value=login />
-    
+
     </form>
     </div>
   </div>
